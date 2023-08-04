7c28bdc5584863f650b9ba65d6537c61e8ffee0f
Abhilasha-R-Teli <teli.abhienjoys@gmail.com>
2023-08-05 01:58:19 +0530

    login changes

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,17 +1,33 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import './LoginPage.css'; 
 
 function LoginPage(){
+    
     return (
-        
-        <>
-            <div className = "loginpage">
-                <h1 className = "loginpage-heading" style={{ textAlign: 'center' }}>RecipeReady</h1>
-                <h2 className = "loginpage-heading" style={{ textAlign: 'center' }}>Login</h2>
+        <div className="login-page">
+            <div className="login-container">
+                <Form>
+                    <h1>RecipeReady</h1>
+                    <h2>Login!</h2>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Phone Number:</Form.Label>
+                        <Form.Control type="email" placeholder="Enter valid phone number" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Enter OTP:</Form.Label>
+                        <Form.Control type="email" placeholder="Enter OTP" />
+                    </Form.Group>
+
+                    <Button type = "submit" variant="outline-light">Submit</Button>
+                </Form>
             </div>
-        </>
-        
+        </div>
     ); 
 }
+
 
 export default LoginPage
