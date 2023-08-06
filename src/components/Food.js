f0b6914d22c2f1acd183b387cc11f13457bbcb87
harini0-0 <harinipri2001@gmail.com>
2023-08-06 08:57:28 +0530

    Co-authored-by: Sadhana Jayakumar <Sjk4824@users.noreply.github.com>

--- src/components/Food.js
+++ src/components/Food.js
@@ -20,35 +20,15 @@ const Food = () => {
         )
     }
 
+    const handleClick = () => {
+        window.location.href = "/itemspecific"; 
+    }
+
     return (
         <div className='max-w-[1240px] m-auto px-4 py-12'>
             <h1 className='text-orange-600 font-bold text-4xl text-center'>
                 Top Rated Menu Items
             </h1>
-            {/* <div className='flex flex-col lg:flex-row justify-between'> */}
-                {/* Filter Type */}
-                {/* <div>
-                    <p className='font-bold text-gray-700'>Filter Type</p>
-                    <div className='flex justify-between flex-wrap'>
-                        <button onClick={() => setFoods(data)} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white border rounded-xl px-5 py-1'>All</button>
-                        <button onClick={() => filterType('burger')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white border rounded-xl px-5 py-1'>Burgers</button>
-                        <button onClick={() => filterType('pizza')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white border rounded-xl px-5 py-1'>Pizza</button>
-                        <button onClick={() => filterType('salad')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white border rounded-xl px-5 py-1'>Salads</button>
-                        <button onClick={() => filterType('chicken')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white border rounded-xl px-5 py-1'>Chicken</button>
-                    </div>
-                </div> */}
-
-                {/* Filter Price */}
-                {/* <div>
-                    <p className='font-bold text-gray-700'>Filter Price</p>
-                    <div className='flex justify-between flex-wrap max-w-[390px] w-full'>
-                        <button onClick={() => filterPrice('$')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white border rounded-xl px-5 py-1'>$</button>
-                        <button onClick={() => filterPrice('$$')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white border rounded-xl px-5 py-1'>$$</button>
-                        <button onClick={() => filterPrice('$$$')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white border rounded-xl px-5 py-1'>$$$</button>
-                        <button onClick={() => filterPrice('$$$$')} className='m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white border rounded-xl px-5 py-1'>$$$$</button>
-                    </div>
-                </div>
-            </div> */}
 
             {/* diplay food */}
             <AnimatePresence>
@@ -62,7 +42,12 @@ const Food = () => {
                             transition={{ duration: 0.1 }}
                             key={item.id}
                             className="border shadow-lg rounded-lg hover:scale-105 duration-500 cursor-pointer">
-                            <img className='w-full h-[150px] md:h-[200px] object-cover rounded-t-lg' src={item.image} alt={item.name} />
+                            <img 
+                            onClick = {(index)=> {window.location.href = `/itemspecific?id=${index}`; }} 
+                            className='w-full h-[150px] md:h-[200px] object-cover rounded-t-lg' 
+                            src={item.image} 
+                            alt={item.name}                                
+                            />
                             <div className='flex justify-between px-2 py-4'>
                                 <p>{item.name}</p>
                                 <p>
---
+++ src/pages/trackerPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Tracker(){
+    return (
+        <div className="wrapper">
+        <div>
+        
+        </div>
+        </div>
+    );
+
+}
+export default Tracker;
